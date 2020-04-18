165e0e61669f4dc0a38ca43994573a782316287d
MickaRz <63889586+MickaRz@users.noreply.github.com>
2020-04-18 10:03:11 +0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>mooh</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="http://localhost/siperpus/asset/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="http://localhost/siperpus/asset/fontawesome/css/all.min.css">
+
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#"><i class="fas fa-book-reader"></i> PERPUSTAKAAN | HAI Micka</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+  <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+        <a class="nav-item nav-link" href="#">Dashboard</a>
+        <a class="nav-item nav-link" href="buku/index.php">Buku</a>
+        <a class="nav-item nav-link" href="anggota/index.php">Anggota</a>
+        <a class="nav-item nav-link" href="transaksi/index.php">Peminjaman</a>
+        <a class="nav-item nav-link" href="#">Logout</a>
+        </div>
+    </div>
+
+</nav>
+
+   <script src="http://localhost/siperpus/aset/jquery.js"></script>
+   <script src="http://localhost/siperpus/aset/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
