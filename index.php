c502aebdbaee92db7c696e2429ec83f8f0902f09
MickaRz <63889586+MickaRz@users.noreply.github.com>
2020-04-18 09:58:27 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+include 'asset/header.php';
+ ?>
+ <div class="container">
+   <div class="row mt-4">
+     <div class="col-md">
+       <h2><i class="fas fa-chart-bar"></i>Dashbor</h2>
+       <hr class="bg-light">
+     </div>
+   </div>
+ </div>
+     <div class="container">
+        <div class="row mt-4">
+   </div>
+ </div>
+ <div class="row">
+
+ <div class="col-md-4">
+                <div class="card bg-danger" style="width: 18rem;">
+                    <div class="card-body text-white">
+                        <h5 class="card-title">jumlah buku</h5>
+                        <p class="card-text" style="font-size:60px">350</p>
+                        <a href="buku/index.php" class="text-white">Lebih detail <i class="fas fa-angle-double-right"></i></a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                            <div class="card bg-warning" style="width: 18rem;">
+                                <div class="card-body text-white">
+                                    <h5 class="card-title">jumlah anggota</h5>
+                                    <p class="card-text" style="font-size:60px">614</p>
+                                    <a href="anggota/index.php" class="text-white">Lebih detail <i class="fas fa-angle-double-right"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                                       <div class="card bg-info" style="width: 18rem;">
+                                           <div class="card-body text-white">
+                                               <h5 class="card-title">jumlah Transaksi</h5>
+                                               <p class="card-text" style="font-size:60px">279</p>
+                                               <a href="transaksi/index.php" class="text-white">Lebih detail <i class="fas fa-angle-double-right"></i></a>
+                                           </div>
+                                       </div>
+                                   </div>
+</div>
+
+ <?php
+include 'asset/footer.php';
+  ?>
